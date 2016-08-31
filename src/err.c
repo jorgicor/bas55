45bd6de8f7703e881c469c7e64143924866681ea
jorgicor <jorge.giner@hotmail.com>
2016-08-31 12:54:07 +0200

    * Deleted NEWS.old (integrated in CHANGELOG.md). * Copyright notice updated in err.c.

--- src/err.c
+++ src/err.c
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2014 Jorge Giner Cordero
+Copyright (c) 2014, 2016 Jorge Giner Cordero
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
